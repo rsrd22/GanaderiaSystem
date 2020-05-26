5c07a0978e56084c1fb907f335c2c7422880bda4
dolfhandler <dolfhandler@gmail.com>
2020-05-25 19:24:24 -0500

    clase agregada

---
+++ src/Utilidades/DireccionPintado.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Utilidades;
+
+/**
+ *
+ * @author DOLFHANDLER
+ */
+public class DireccionPintado {
+    public static final int DEFAULT = 0;
+    public static final int ABAJO = 0;
+    public static final int ARRIBA = 1;
+}
