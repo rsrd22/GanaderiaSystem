8a92af46e90e3394838feab518d81f407800c974
dolfhandler <dolfhandler@gmail.com>
2020-09-05 12:39:56 -0500

    modelo y control de perfiles

---
+++ src/Control/Usuario/ControlPerfiles.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Control.Usuario;
+
+import Control.IControl;
+
+/**
+ *
+ * @author DOLFHANDLER
+ */
+public class ControlPerfiles implements IControl {
+
+    @Override
+    public Object ObtenerDatos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object ObtenerDatosKey(String ID) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int Guardar(Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int Actualizar(Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int Eliminar(Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object ObtenerDatosFiltro(Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
